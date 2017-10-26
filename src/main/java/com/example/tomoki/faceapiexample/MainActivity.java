cc1564ee98811b06e540c5d8e378a95ae253ce1b
tomoki <monndenn15@gmail.com>
2017-10-26 17:11:08 +0900

    AsyncHttpRequestの呼び出しを追加

--- src/main/java/com/example/tomoki/faceapiexample/MainActivity.java
+++ src/main/java/com/example/tomoki/faceapiexample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tomoki.faceapiexample;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Select(View v) {
+        // httpリクエストを入れる変数
+        Uri.Builder builder = new Uri.Builder();
+        AsyncHttpRequest task = new AsyncHttpRequest(this);
+        task.execute(builder);
     }
 }
